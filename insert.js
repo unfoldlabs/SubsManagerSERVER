50c330172bd92f6363b3380b48fa7f1434a9e2eb
unfoldlabs <unfoldlabs@users.noreply.github.com>
2016-10-03 11:00:40 -0700

    Removed Blank Spaces

--- insert.js
+++ insert.js
@@ -1,7 +1,3 @@
-/**
- * http://usejsdoc.org/
- */
-
 var http = require('http');
 var https = require('https');
 var express = require('express');
@@ -10,13 +6,11 @@ var mysql = require('mysql');
 var router = express.Router();
 var url = require('url');
 
-
 var connection = mysql.createConnection({
 	host:'localhost',
 	user:'root',
 	//password:'root',
-	database:'db'
-		
+	database:'db'	
 });
 
 connection.connect(function(err){
@@ -24,47 +18,38 @@ connection.connect(function(err){
 		console.error(err);
 		console.log('error');
 	}
-	
 	else{
 		console.log('connected');
 	}
-	
 });
-var queryString_Initial = 'Use db;';
-connection.query(queryString_Initial, function(err, rows, fields){
-	if(err){
-		console.error(err);
-		throw err;
-	}
-	else{
-		console.log("Database used");
-	}
-});
-//app.use('/', router);
-app.get('/insert', function(req, res) {
-var emailid = req.query.email;
-var password = req.query.Password;
-
+	var queryString_Initial = 'Use db;';
+	connection.query(queryString_Initial, function(err, rows, fields){
+		if(err){
+			console.error(err);
+			throw err;
+		}
+		else{
+			console.log("Database used");
+		}
+	});
+	app.get('/insert', function(req, res) {
+	var emailid = req.query.email;
+	var password = req.query.Password;
 	var queryString = 'Insert into Users(EmailID,Password) values("'+emailid+'","'+password+'")';
-connection.query(queryString, function(err, rows, fields) {
-    if (err){
-    	throw err;
-    	//console.log("error");
-    }
-    else{
-    	console.log("Users Table : Data Inserted");
-    }
-    connection.end();
-    });
-res.send("sent: "+emailid+" "+password);
-});
-
-
-
-
-module.exports = router;
-//connection.end();
-app.listen(1400);
+	connection.query(queryString, function(err, rows, fields) {
+	    if (err){
+		throw err;
+		//console.log("error");
+	    }
+	    else{
+		console.log("Users Table : Data Inserted");
+	    }
+    		connection.end();
+    	});
+	res.send("sent: "+emailid+" "+password);
+	});
+	module.exports = router;
+	app.listen(1400);
 
 
 
